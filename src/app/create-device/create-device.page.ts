37fb2a1c96e03f5d51dbf40d76df90c81ddd7c0f
Jonatan Gall <jonatangd.souza@gmail.com>
2021-06-18 18:29:43 -0300

    layout issues

--- src/app/create-device/create-device.page.ts
+++ src/app/create-device/create-device.page.ts
@@ -9,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class CreateDevicePage implements OnInit {
   description = new FormControl('', [Validators.required]);
   location = new FormControl('', [Validators.required]);
+  bucket = new FormControl('', [Validators.required]);
   id = new FormControl('', [Validators.required]);
   constructor() { }
 
   ngOnInit() {
   }
 
+  get disabled() {
+    return !this.description.valid || !this.id.valid || !this.location.valid || !this.bucket.valid;
+  }
 }
